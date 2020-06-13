e9280fb0cb4d85cdc2ba3b4bd7876f9c884d9105
porag-m06 <porag.m06@gmail.com>
2020-06-13 14:17:00 +0600

    second commit

---
+++ basic1variables.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Basics One</title>
+    <link rel="stylesheet" href="styletest.css"/>
+</head>
+<body>
+    <h1>Welcome to PHP basics : Intro & Variables</h1>
+    <Div>
+    <h2 class="line">___________________________________</h2>
+        <h2 class="heading-2">Introduction :</h2>
+        
+        <P>
+            PHP stands for  "PHP : Hypertext Preprocessor". 
+            A recursive acronym. It's general purpose, scriptiong
+            language. 
+            It's best suited for web development. 
+            <h4 style="color:skyblue;">Php is created by RASMUS LERDORF in 1994. </h4> <hr>
+            It's server side language that means it runs on a server
+            <span class="heading-2">(e. g. Apache web server)</span>. 
+
+        </P>
+    </Div>
+
+    <Div>
+    <h2 class="line">____________________________________</h2>
+    <h2 class="heading-2">Variables :</h2>
+
+    <P>Types : 
+        <ol>
+            <li>Integers</li>
+            <li>Floats</li>
+            <li>Strings</li>
+            <li>Boolean</li>
+            <li>Arrays</li>
+            <li>Obkects</li>
+            <li>Resources</li>
+            <li>Null</li>
+        </ol>
+    
+
+    </P>
+
+    </Div>
+</body>
+</html>
---
+++ info.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>INFO PHP</title>
+</head>
+<body>
+   <?php 
+   //This only the php info file
+   phpinfo();
+   ?> 
+</body>
+</html>
